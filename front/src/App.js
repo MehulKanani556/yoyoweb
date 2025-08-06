c488d66ba4930db2833a1b8c04624eb63b8131cd
jaykalathiya2005 <jaykalathiyainfotech@gmail.com>
2025-08-06 14:50:36 +0530

    installation

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
